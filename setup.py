630fa456020a866da8e6cb37ff23fbe7fba4420e
sina <sina.mansour.lakouraj@gmail.com>
2022-05-27 01:40:28 +1000

    resolved compatiblity issues in the previous build

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = ["numpy", "scipy", "sklearn", "nibabel", "gdist"]
 
 setuptools.setup(
     name="Connectome_Spatial_Smoothing",
-    version="0.1.2",
+    version="0.1.3",
     author="Sina Mansour L.",
     author_email="sina.mansour.lakouraj@gmail.com",
     description="Connectome Spatial Smoothing",
